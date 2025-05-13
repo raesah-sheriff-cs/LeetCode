e6560a032a6348e09bfa7ae95eb7c0df423c4d91
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2025-05-12 20:38:46 -0600

    LC-second-highest-salary [Runtime: N/A; Memory: N/A]

--- database/second-highest-salary.sql
+++ database/second-highest-salary.sql
@@ -1,1 +1,7 @@
 /* Write your T-SQL query statement below */
+SELECT MAX(salary) AS SecondHighestSalary
+FROM (
+    SELECT salary, RANK() OVER (ORDER BY salary DESC) AS r
+    FROM Employee
+) ranked
+WHERE r = 2;
