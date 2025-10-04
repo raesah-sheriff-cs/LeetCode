21d5db7db53598f901a44d34e73dc8cfc8475117
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2025-10-04 13:54:25 -0600

    LC-investments-in-2016 [Runtime: N/A; Memory: N/A]

---
+++ database/investments-in-2016.sql
@@ -0,0 +1,7 @@
+with t as 
+(SELECT tiv_2015 from Insurance group by tiv_2015 having count(*)>1)
+,l as (SELECT lat, lon from Insurance group by lat, lon having count(*)=1)
+SELECT sum(tiv_2016) as tiv_2016 from Insurance i join t on i.tiv_2015=t.tiv_2015
+join l on l.lat=i.lat and l.lon=i.lon
+
+
