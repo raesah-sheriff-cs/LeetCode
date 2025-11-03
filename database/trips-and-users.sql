40dd5640af967dbc0eb515f06b93b4a2ca119680
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2025-11-03 06:22:34 -0700

    LC-trips-and-users [Runtime: 279 ms; Memory: 0B]

--- database/trips-and-users.sql
+++ database/trips-and-users.sql
@@ -1,14 +1,14 @@
 
 with unbanned_client as(
 SELECT id,status, banned,request_at,
-case when status like 'cancelled%' then 1 else 0 end as cc from Trips t join Users u on t.client_id=u.users_id and banned='No'
+case when status like 'cancelled%' then 1 else 0 end as cc from Trips t join Users u on t.client_id=u.users_id and banned='No' and request_at between '2013-10-01' and '2013-10-03'
 )
 ,unbanned_driver as(
 SELECT id,status, banned,request_at,case when status like 'cancelled%' then 1 else 0 end as cc
-from Trips t join Users u on t.driver_id=u.users_id and banned='No'
+from Trips t join Users u on t.driver_id=u.users_id and banned='No' and request_at between '2013-10-01' and '2013-10-03'
 )
 SELECT uc.request_at as Day, 
-round(sum(uc.cc)*1.0/count(uc.cc),2) as [Cancellation Rate]
+round(sum(ud.cc)*1.0/count(ud.cc),2) as [Cancellation Rate]
 from unbanned_client uc
 join unbanned_driver ud on uc.id=ud.id
 group by uc.request_at
