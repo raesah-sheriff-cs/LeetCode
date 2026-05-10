6470f94aab4a1d0508d897b8574812a4115b6535
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2026-05-10 16:25:53 -0600

    LC-path-crossing [Runtime: N/A; Memory: N/A]

--- string/path-crossing.py
+++ string/path-crossing.py
@@ -1,30 +1,23 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        d={}
-        
-        O=[0,0]
-        N=[1,0]
-        S=[-1,0]
-        E=[0,1]
-        W=[0,-1]
-
+       
         x=0
         y=set()
         y.add(tuple(O))
        
         while(x<=len(path)-1):
             if path[x]=='N':
-                O[0]=O[0]+N[0]
-                O[1]=O[1]+N[1]
+                O[0]=O[0]+1
+                #O[1]=O[1]+0
             if path[x]=='S':
-                O[0]=O[0]+S[0]
-                O[1]=O[1]+S[1]
+                O[0]=O[0]-1
+                #O[1]=O[1]+0
             if path[x]=='E':
-                O[0]=O[0]+E[0]
-                O[1]=O[1]+E[1]
+                #O[0]=O[0]+E[0]
+                O[1]=O[1]+1
             if path[x]=='W':
-                O[0]=O[0]+W[0]
-                O[1]=O[1]+W[1]
+                #O[0]=O[0]+0
+                O[1]=O[1]-1
             
             if tuple(O) in y:
                 return True
