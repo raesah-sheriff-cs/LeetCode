13a9a05d7662680fbbb6acb5ffd31ab87c6d6443
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2026-05-10 13:57:28 -0600

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

---
+++ stack/backspace-string-compare.py
@@ -0,0 +1,21 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1=[]
+        s2=[]
+        x=0
+        y=0
+        while x<=len(s)-1:
+            if s[x]!='#':
+                s1.append(s[x])
+            else:
+                s1.pop()
+            x=x+1
+        while y<=len(t)-1:
+            if t[y]!='#':
+                s2.append(t[y])
+            else:
+                s2.pop()
+            y=y+1
+        if s1==s2:
+            return True
+        return False
