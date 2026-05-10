ec8459447561cd2f64d20fb95f8f94da010cd174
raesah-sheriff-cs <161255968+raesah-sheriff-cs@users.noreply.github.com>
2026-05-10 14:04:08 -0600

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- simulation/backspace-string-compare.py
+++ simulation/backspace-string-compare.py
@@ -4,18 +4,28 @@ class Solution:
         s2=[]
         x=0
         y=0
+        c=0
+        d=0
         while x<=len(s)-1:
             if s[x]!='#':
                 s1.append(s[x])
             else:
-                s1.pop()
+                c+=1
             x=x+1
         while y<=len(t)-1:
             if t[y]!='#':
                 s2.append(t[y])
             else:
-                s2.pop()
+                d+=1
             y=y+1
+        
+        while c>0:
+            s1.pop()
+            c=c-1
+        while d>0:
+            s2.pop()
+            d=d-1
+            
         if s1==s2:
             return True
         return False
